34d85cae1223587726e4f3646d5b1b56b9403c31
Flow <florian.gustin.barriera@gmail.com>
2019-11-06 13:28:50 +0100

    stack for drawer + icon button

--- my_project/src/navigations/index.js
+++ my_project/src/navigations/index.js
@@ -1,19 +1,30 @@
+import * as React from 'react';
+import { IconButton, Colors } from 'react-native-paper';
 import {
     createDrawerNavigator
 } from 'react-navigation-drawer';
-
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
-
-
-
 
 import HomeSearchScreen from "../views/HomeSearchScreen"
 import MovieDetailScreen from "../views/MovieDetailScreen"
 import AboutScreen from "../views/AboutScreen"
 import ProfileScreen from "../views/ProfileScreen"
 
-const drawerNavigator = createDrawerNavigator({
+const MyHamburger = () => (
+    <IconButton
+        icon="menu"
+        color={Colors.red500}
+        size={20}
+        onPress={() => console.log('Pressed')}
+    />
+);
+
+
+
+
+
+const DrawerNavigator = createDrawerNavigator({
     Home: {
         screen: HomeSearchScreen,
     },
@@ -34,7 +45,19 @@ const homeStack = createStackNavigator({
     }
 });
 
+const MainStack = createStackNavigator({
+    DrawerNavigator : {
+        screen : DrawerNavigator,
+        navigationOptions: ({ navigation }) => ({
+        header : MyHamburger
+        })
+    }
+});
 
-const DrawerNavigation = createAppContainer(drawerNavigator);
+// const DrawerNavigation = createAppContainer(DrawerNavigator);
+const DrawerNavigation = createAppContainer(MainStack);
+
 
 export default DrawerNavigation;
+
+
