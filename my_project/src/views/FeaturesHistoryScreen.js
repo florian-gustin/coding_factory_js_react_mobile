c8dfbc35d0381b227c9a39ddb868b3d50890de48
Gustin Florian <florian.gustin.business@gmail.com>
2019-11-28 20:06:14 +0100

    features dones

--- my_project/src/views/FeaturesHistoryScreen.js
+++ my_project/src/views/FeaturesHistoryScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {Animated, SafeAreaView, ScrollView, Text, View} from 'react-native';
 import {IconButton, Colors, Drawer, List, Divider} from 'react-native-paper';
+import * as Animatable from 'react-native-animatable';
 
 import i118n from '../components/i118n';
 
@@ -33,90 +34,68 @@ const FadeInView = (props, duration) => {
 
 const FeaturesHistoryScreen = () => {
 
+    const allFeatures = [
+        "form submittion",
+        "auth firebase",
+        "store firestore",
+        "store redux",
+        "call API",
+        "list of favorites",
+        "video player for trailer",
+        "bottom bar",
+        "advanced drawer navigation",
+        "languages EN/FR",
+        "dark mode",
+        "animations",
+        "loading mode",
+        "star icon color status",
+        "hooks",
+        "UX approach"
+    ]
+
+    const myAnimation = (iterator, text, delay) => {
+        return (
+            <>
+                <Animatable.Text animation="fadeInDown" delay={1000*delay} iterationCount={1} direction="normal"
+                                 style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}
+                >{iterator} - {text}
+                </Animatable.Text>
+                <Divider />
+            </>
+        )
+    }
+
+
     return (
         <ScrollView >
-            <Text style={{fontSize: 18, textAlign: 'center', margin: 30, textTransform: 'uppercase', fontWeight : 'bold', color: 'indigo'}}>This app was made without any boilerplate</Text>
-
             <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>1 - form submittion</Text>
-                <Divider />
-            </FadeInView>
-
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>2 - auth firebase</Text>
-                <Divider />
+                <Text style={{
+                    fontSize: 18, textAlign: 'center', textTransform: 'uppercase', fontWeight : 'bold', color: 'indigo'
+                }}
+                >This app was made without any boilerplate</Text>
 
             </FadeInView>
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>3 - store firestore</Text>
-                <Divider />
+            <View style={{marginTop: 20}}>
 
-            </FadeInView>
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>4 - store redux</Text>
-                <Divider />
+                {myAnimation(1, allFeatures[0], 1)}
+                {myAnimation(2, allFeatures[1], 2)}
+                {myAnimation(3, allFeatures[2], 3)}
+                {myAnimation(4, allFeatures[3], 4)}
+                {myAnimation(5, allFeatures[4], 5)}
+                {myAnimation(6, allFeatures[5], 6)}
+                {myAnimation(7, allFeatures[6], 7)}
+                {myAnimation(8, allFeatures[7], 8)}
+                {myAnimation(9, allFeatures[8], 9)}
+                {myAnimation(10, allFeatures[9], 10)}
+                {myAnimation(11, allFeatures[10], 11)}
+                {myAnimation(12, allFeatures[11], 12)}
+                {myAnimation(13, allFeatures[12], 13)}
+                {myAnimation(14, allFeatures[13], 14)}
+                {myAnimation(15, allFeatures[14], 15)}
+                {myAnimation(16, allFeatures[15], 16)}
+            </View>
 
-            </FadeInView>
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>5 - call API</Text>
-                <Divider />
 
-            </FadeInView>
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>6 - list of favorites</Text>
-                <Divider />
-
-            </FadeInView>
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>7 - video player for trailer</Text>
-                <Divider />
-
-            </FadeInView>
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>8 - bottom bar</Text>
-                <Divider />
-
-            </FadeInView>
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>9 - advanced drawer navigation</Text>
-                <Divider />
-
-            </FadeInView>
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>10 - languages EN/FR</Text>
-                <Divider />
-
-            </FadeInView>
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>11 - dark mode</Text>
-                <Divider />
-
-            </FadeInView>
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>12 - animations</Text>
-                <Divider />
-
-            </FadeInView>
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>13 - loading mode</Text>
-                <Divider />
-
-            </FadeInView>
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>14 - star icon color status</Text>
-                <Divider />
-
-            </FadeInView>
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>15 - hooks</Text>
-                <Divider />
-
-            </FadeInView>
-            <FadeInView style={{height: 50}}>
-                <Text style={{fontSize: 12, textAlign: 'center', margin: 10, textTransform: 'uppercase', fontWeight : 'bold'}}>16 - UX</Text>
-                <Divider />
-
-            </FadeInView>
         </ScrollView>
 
     )
