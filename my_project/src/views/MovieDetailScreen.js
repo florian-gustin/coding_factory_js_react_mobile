783b761f107391edcc37b5e4f4b9cf0a42afef15
Cédric <cedric.leprohon@yahoo.fr>
2019-11-28 15:36:32 +0100

    video

--- my_project/src/views/MovieDetailScreen.js
+++ my_project/src/views/MovieDetailScreen.js
@@ -1,17 +1,42 @@
-import React, { } from 'react';
+import React, { useState } from 'react';
 import { Text, View, ImageBackground } from 'react-native';
 import { Paragraph, Divider } from 'react-native-paper';
 import {ScrollView} from 'react-navigation';
-import { useNavigationParam } from 'react-navigation-hooks'
+import { useNavigationParam } from 'react-navigation-hooks';
+import { getTrailersFromMovie } from '../helpers/vendors/TMDB';
+import YouTube from 'react-native-youtube';
+import { YOUTUBE_API_KEY } from '../helpers/vendors/YoutubeApiKey';
 
 
 const DetailsScreen = () => {
   const data = useNavigationParam('data');
 
+  const [movie, setMovie] = useState(0);
+
+  getTrailersFromMovie(data.id).then((res) => {
+    if(res.results.length > 0) {
+      setMovie(res.results[0].key);
+      console.log("movie", movie);
+
+    }
+  });
+
+
   return (
     <ScrollView style={{ flex: 1, width: '100%', padding: 5 }}>
-      <ImageBackground source={{ uri: data.poster }} style={{width: '100%', height: 400}}>
+      {movie != 0 ? (
+              <YouTube
+              apiKey={YOUTUBE_API_KEY}
+              videoId={movie} // The YouTube video ID
+              play // control playback of video with true/false
+              // control whether the video should play in fullscreen or inline
+              loop // control whether the video should loop when ended
+              style={{ alignSelf: 'stretch', height: 300 }}
+              />
+      ) : (
+        <ImageBackground source={{ uri: data.poster }} style={{width: '100%', height: 400}}>
       </ImageBackground>
+      )}
       <View style={{flexDirection: 'row', justifyContent: 'space-between', padding: 10}}>
         <Text style={{fontSize: 20}}>{data.title}</Text>
         <Text style={{fontSize: 14}}>{data.date}</Text>
